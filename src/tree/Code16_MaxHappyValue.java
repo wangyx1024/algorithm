642bda92fc23903ebe03a942d51dd70aca1e037c
王雁翔 <1668171078@qq.com>
2021-04-06 00:55:02 +0800

    max happy value

---
+++ src/tree/Code16_MaxHappyValue.java
@@ -0,0 +1,28 @@
+package tree;
+
+import datastruct.BinaryTreeNode;
+import util.U;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * 最大快乐值问题
+ */
+public class Code16_MaxHappyValue {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void check(int times, int maxTreeLevel, int maxNodeValue) {
+
+    }
+
+    private static class Info {
+
+    }
+}
