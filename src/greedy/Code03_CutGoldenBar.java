074a86ad61c37916b7b37dc3d922d8a15991b206
王雁翔 <1668171078@qq.com>
2021-04-10 15:51:53 +0800

    2 greedy questions

---
+++ src/greedy/Code03_CutGoldenBar.java
@@ -0,0 +1,18 @@
+package greedy;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 切金条问题
+ */
+public class Code03_CutGoldenBar {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
