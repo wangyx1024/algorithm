dc44b92ab61690df3c66f187a8d0e0435568a04d
王雁翔 <1668171078@qq.com>
2021-04-10 17:30:52 +0800

    cut gold bar cost

--- src/greedy/Code03_CutGoldenBar.java
+++ src/greedy/Code03_CutGoldenBar.java
@@ -1,9 +1,7 @@
 package greedy;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * 切金条问题
@@ -11,8 +9,43 @@ import java.util.Random;
 public class Code03_CutGoldenBar {
 
     public static void main(String[] args) {
-
+        int[] arr = {10, 20, 30};
+        int cost = greedySolve(arr);
+        System.out.println("cost = " + cost);
     }
 
+    private static class MinComparator implements Comparator<Integer> {
 
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1 - o2;
+        }
+    }
+
+    // todo
+    private static int violentSolve(int[] arr) {
+        return 0;
+    }
+
+    private static int greedySolve(int[] arr) {
+        if (arr == null) {
+            return 0;
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue(new MinComparator());
+        for (int i : arr) {
+            queue.add(i);
+        }
+
+        int cost = 0;
+        while (queue.size() > 1) {
+            int c1 = queue.poll();
+            int c2 = queue.poll();
+            int c = c1 + c2;
+            queue.add(c);
+            cost += c;
+        }
+
+        return cost;
+    }
 }
